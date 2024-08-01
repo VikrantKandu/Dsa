ca4f0c2632e21144081ebdf1cb584bddaf813928
VikrantKandu <vikrantkandu14091998@gmail.com>
2024-08-01 20:57:55 +0530

    first spring boot

---
+++ src/main/java/com/example/kanhaiyasingh/jadu.java
@@ -0,0 +1,4 @@
+package com.example.kanhaiyasingh;
+
+public class jadu {
+}
---
+++ src/test/java/com/example/kanhaiyasingh/KanhaiyasinghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanhaiyasingh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanhaiyasinghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
